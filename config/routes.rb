cdcf866f371278047252bfae1d4111a4a8d47481
Vincent Ramos <vince9322@gmail.com>
2022-05-16 11:22:30 +0200

    Exercise 2 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'ask', to: 'pages#ask'
+  get 'answer', to: 'pages#answer'
   # Defines the root path route ("/")
   # root "articles#index"
 end
